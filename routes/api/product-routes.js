f19d19f6ad0d14a674c6fde2b6ce71d42b8fcfeb
araceli <carmona.araceli.4690@gmail.com>
2021-09-22 17:26:26 -0700

    product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -5,27 +5,52 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 
 // get all products
 router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+  // find all products & its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+      model: Category,
+      attributes: ['category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name']
+    }]
+  }).then(dbProductData => {
+    res.json(dbProductData)
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    attributes: ['id', 'product_name', 'price', 'stock'],
+    include: [{
+      model: Category,
+      attributes: ['category_name']
+    },
+    {
+      model: Tag,
+      attributes: ['tag_name']
+    }
+    ]
+  }).then(dbProductData => {
+    res.json(dbProductData)
+  })
 });
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+
+  Product.create(req.body)({
+    product_name: req.params.product_name,
+    price: req.params.price,
+    stock: req.params.stock,
+    category_id: req.params.category_id,
+    tagIds: req.params.tagIds
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -91,6 +116,13 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbProductData => {
+    res.json(dbProductData)
+  })
 });
 
 module.exports = router;
